dc29983121063f4ac283e0b10b81c4024f574e94
hitalo <hitalova@gmail.com>
2021-02-07 18:57:16 -0300

    verify Token not provided

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,12 +3,17 @@ const { SECRET_KEY } = process.env;
 
 module.exports = {
     async verifyJWT(req, res, next) {
+
+        if (!req.headers.authorization) {
+            return res.status(401).json({ massage: "Token not provided" });
+        }
+
         const [, authorization] = req.headers.authorization.split(' ');
 
         jwt.verify(authorization, SECRET_KEY, (err, decoder) => {
 
             if (err) {
-                return res.status(401).json({ massage: " Not Authorized" });
+                return res.status(401).json({ massage: "Not Authorized" });
             }
 
             req.userId = decoder.userId;
